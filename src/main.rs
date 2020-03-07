2c84987a466cf2b0e5fbe71873b7827302b1487e
Andrei Dobre <andreidobre.web@gmail.com>
2020-03-07 13:41:46 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/about")]
+fn about() -> &'static str {
+    "About me"
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, about]).launch();
 }
